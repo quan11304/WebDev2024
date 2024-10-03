583b5991000c10896c25d45ea15a5f96b4d9582e
191204taianh <anhnt.22bi13028@usth.edu.vn>
2024-10-04 06:32:20 +0700

    Update Login + Register page

--- LoginRegister page/login.js
+++ LoginRegister page/login.js
@@ -1,10 +1,46 @@
 document.getElementById('loginBtn').addEventListener('click', function () {
-    var email = document.getElementById('email').value;
-    var password = document.getElementById('password').value;
+    loginUser();
+});
 
-    if (email === 'AnhNT@gmail.com' && password === 'AnhNT!') {
+document.addEventListener('keydown', function (event) {
+    if (event.key === 'Enter') {
+        loginUser();
+    }
+});
+
+function loginUser() {
+    // Get input values from login form
+    var inputUsernameOrEmail = document.getElementById('usernameoremail').value;
+    var inputPassword = document.getElementById('password').value;
+
+    // Retrieve stored email, username, and password from localStorage
+    var storedUsername = localStorage.getItem('username');
+    var storedEmail = localStorage.getItem('email');
+    var storedPassword = localStorage.getItem('password');
+
+    // Check if the input matches the stored data
+    if ((inputUsernameOrEmail === storedEmail || inputUsernameOrEmail === storedUsername) && inputPassword === storedPassword) {
         window.location.href = '/Code/WebDev2024/Home page/home.html';
     } else {
-        alert('Invalid email or password!');
+        alert('Username/Email or Password is invalid!');
+    }
+}
+
+// Clear Username/Email
+document.getElementById('clearUsernameEmail').addEventListener('click', function () {
+    document.getElementById('usernameoremail').value = '';
+});
+
+// Toggle Password Visibility
+document.getElementById('viewPassword').addEventListener('click', function () {
+    var passwordInput = document.getElementById('password');
+    var passwordIcon = document.getElementById('viewPassword');
+    
+    if (passwordInput.type === 'password') {
+        passwordInput.type = 'text';
+        passwordIcon.src = '../Icons/hide.png'; // Change to 'hide' icon
+    } else {
+        passwordInput.type = 'password';
+        passwordIcon.src = '../Icons/view.png'; // Change back to 'view' icon
     }
 });
